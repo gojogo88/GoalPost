c7312663c5b7cd9b3753a67271c3dc9a4f4b5b80
Jonathan Go <jogo88@gmail.com>
2017-09-11 13:28:20 +0900

    fetching data from persistent store and displaying in the tableview

--- GoalPost/Controller/GoalsVC.swift
+++ GoalPost/Controller/GoalsVC.swift
@@ -10,11 +10,15 @@ import UIKit
 import CoreData
 
 let appDelegate = UIApplication.shared.delegate as? AppDelegate
+//references the existing appDelegate
 
 class GoalsVC: UIViewController {
 
     //Outlets
     @IBOutlet weak var tableView: UITableView!
+    
+    //variables
+    var goals = [Goal]()
     
     
     override func viewDidLoad() {
@@ -24,13 +28,25 @@ class GoalsVC: UIViewController {
         tableView.delegate = self
         tableView.isHidden = false
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.fetchData { (complete) in
+            if complete {
+                if goals.count >= 1 {
+                    tableView.isHidden = false
+                } else {
+                    tableView.isHidden = true
+                }
+            }
+        }
+        tableView.reloadData()
+    }
 
     @IBAction func addGoalBtnPressed(_ sender: Any) {
         guard let createGoalVC = storyboard?.instantiateViewController(withIdentifier: "CreateGoalVC") else { return }
         presentDetail(createGoalVC)
     }
-    
-
 
 }
 
@@ -40,13 +56,36 @@ extension GoalsVC: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return goals.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else { return UITableViewCell() }
-        cell.configureCell(description: "Eat Salad thrice a week.", type: .shortTerm, goalProgressAmount: 3)
+        
+        let goal = goals[indexPath.row]
+        
+        cell.configureCell(goal: goal)
         return cell
         
+    }
+}
+
+extension GoalsVC {
+    func fetchData(completion: (_ complete: Bool) ->()) {
+        //1.Create an constant to hold the Managed Object Context
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        
+        //2.Create a fetch request to grab data from the persistent storage
+        let fetchRequest = NSFetchRequest<Goal>(entityName: "Goal")
+        
+        //3.Used our constant to fetch all that data from the fetch request
+        do {
+            goals = try managedContext.fetch(fetchRequest)
+            print("Successfully fetched data")
+            completion(true)
+        } catch {
+            debugPrint("Could not fetch \(error.localizedDescription)")
+            completion(false)
+        }
     }
 }
