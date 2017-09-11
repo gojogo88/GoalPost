6cb020e2ee616bc97a06bbef0d7d002139ed4646
Jonathan Go <jogo88@gmail.com>
2017-09-11 13:47:06 +0900

    removing objects from persistent store with delete action

--- GoalPost/Controller/GoalsVC.swift
+++ GoalPost/Controller/GoalsVC.swift
@@ -31,6 +31,11 @@ class GoalsVC: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        fetchCoreDataObjects()
+        tableView.reloadData()
+   }
+    
+    func fetchCoreDataObjects() {
         self.fetchData { (complete) in
             if complete {
                 if goals.count >= 1 {
@@ -40,7 +45,6 @@ class GoalsVC: UIViewController {
                 }
             }
         }
-        tableView.reloadData()
     }
 
     @IBAction func addGoalBtnPressed(_ sender: Any) {
@@ -66,7 +70,28 @@ extension GoalsVC: UITableViewDataSource, UITableViewDelegate {
         
         cell.configureCell(goal: goal)
         return cell
+    }
+    
+    //to be able to edit our tableview
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    //editing style
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return .none
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "DELETE") { (rowAction, indexPath) in
+            self.removeGoal(atIndexPath: indexPath)
+            self.fetchCoreDataObjects()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
         
+        deleteAction.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+        
+        return [deleteAction]
     }
 }
 
@@ -88,4 +113,19 @@ extension GoalsVC {
             completion(false)
         }
     }
+    
+    func removeGoal(atIndexPath indexPath: IndexPath) {
+        //1.Create an constant/reference to hold the Managed Object Context
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        
+        managedContext.delete(goals[indexPath.row])
+        
+        do {
+            try managedContext.save()
+            print("Successfully removed goal")
+        } catch {
+            debugPrint("Could not remove \(error.localizedDescription)")
+        }
+    }
+    
 }
