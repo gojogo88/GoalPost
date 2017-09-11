d881158bf457b55e124cb283b2bef25b612f31ba
Jonathan Go <jogo88@gmail.com>
2017-09-11 11:27:24 +0900

    added extensions for keyboardbinding and goaltype buttons

---
+++ GoalPost/Extensions/UIViewExt.swift
@@ -0,0 +1,29 @@
+//
+//  UIViewExt.swift
+//  GoalPost
+//
+//  Created by Jonathan Go on 2017/09/11.
+//  Copyright © 2017 Appdelight. All rights reserved.
+//
+
+//swift file
+import UIKit
+
+extension UIView {
+    //adds an observer
+    func bindToKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+    @objc func keyboardWillChange(_ notification: NSNotification) {
+        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as! UInt
+        let startingFrame = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue   //converts it to raw data
+        let endingFrame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let deltaY = endingFrame.origin.y - startingFrame.origin.y
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIViewKeyframeAnimationOptions(rawValue: curve), animations: {
+            self.frame.origin.y += deltaY  //moving it with how high the keyboard is
+        }, completion: nil)
+    }
+}
