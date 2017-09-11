2abf15ec4b9c08c55434a718a4ceec8ef822108d
Jonathan Go <jogo88@gmail.com>
2017-09-11 09:47:52 +0900

    finished layout

--- GoalPost/Controller/GoalsVC.swift
+++ GoalPost/Controller/GoalsVC.swift
@@ -8,17 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addGoalBtnPressed(_ sender: Any) {
+        print("button was pressed")
     }
+    
 
 
 }
---
+++ GoalPost/View/GoalCell.swift
@@ -0,0 +1,25 @@
+//
+//  GoalCell.swift
+//  GoalPost
+//
+//  Created by Jonathan Go on 2017/09/11.
+//  Copyright © 2017 Appdelight. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    //Outlets
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+    
+}
